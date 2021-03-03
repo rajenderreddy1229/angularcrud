53945afd8ea490b5e167bf52df8479a806052519
unknown <rajenderreddy1229@gmail.com>
2021-03-03 17:41:41 +0530

    initial commit

---
+++ src/app/products.service.ts
@@ -0,0 +1,22 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ProductsService {
+  uri = 'http://localhost:4000/products';
+
+  constructor(private http: HttpClient) { }
+  addProduct(ProductName:any, ProductDescription:any, ProductPrice:any) {
+    const obj = {
+      ProductName,
+      ProductDescription,
+      ProductPrice
+    };
+    // console.log(obj);
+    this.http.post(`${this.uri}/add`, obj)
+      .subscribe(res => console.log('Done'));
+  }
+}
